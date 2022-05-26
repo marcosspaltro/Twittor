4916f6b7d051169cd069f288ae48436068aa40ba
marcosspaltro <msspaltro@hotmail.com>
2022-05-26 14:47:14 -0300

    modificamos laa

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 importScripts('/js/sw-utils.js');
 
 const SATATIC_CACHE = 'static-v1';
-const DYNAMIC_CACHE = 'dynamic-v2';
+const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 
@@ -42,22 +42,25 @@ self.addEventListener('install', e => {
 
 
 self.addEventListener('activate', e => {
-
-    const respuesta = caches.keys().then(keys => {
-
-        keys.forEach(key => {
-
-            if (key !== SATATIC_CACHE && key.includes('static')) {
+ 
+    const respuesta = caches.keys().then( keys => {
+ 
+        keys.forEach( key => {
+ 
+            if (  key !== STATIC_CACHE && key.includes('static') ) {
                 return caches.delete(key);
             }
-
-            if (key !== DYNAMIC_CACHE && key.includes('dynamic')) {
+ 
+            if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
                 return caches.delete(key);
             }
+ 
         });
+ 
     });
-
-    e.waitUntil(respuesta);
+ 
+    e.waitUntil( respuesta );
+ 
 });
 
 self.addEventListener('fetch', e => {
